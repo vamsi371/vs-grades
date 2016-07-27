655ac1c76f7b451a2869c88b4ce560b495024619
vamsi371 <vamsipv.desu37@live.com>
2016-07-27 10:50:24 +0530

    understand <:> used along constructors to pass arguments

---
+++ grades/ThrowawayGradebook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grades
+{
+    
+    class ThrowawayGradebook : Gradebook
+    {
+        //<:> is helpful to pass the control
+        //to some other constructor 
+
+        //for example here in this class
+        //the first constructor passes name to <this> which is
+        //second constructor and it again passes name to
+        //base class(Gradebook), so finally Gradebook is constructed with name
+        
+        //float num is nothing ,just for creating two different constructors
+        public ThrowawayGradebook(string name, float num) : this (name)
+        {
+            
+        }
+            public ThrowawayGradebook(string name): base (name)
+        {
+                
+        }
+        
+    }
+}
